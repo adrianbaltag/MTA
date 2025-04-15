4f86eec133c4b609d46bb1a1f382eedb66064859
adrianbaltag <79178876+adrianbaltag@users.noreply.github.com>
2025-04-15 18:46:43 -0400

    Update config.py

--- src/my_package/utils/config.py
+++ src/my_package/utils/config.py
@@ -3,4 +3,4 @@
 URL_GOOGLE = "https://www.google.com"
 URL_YOUTUBE = "https://www.youtube.com"
 URL_FORMY = "https://formy-project.herokuapp.com/form"
-URL_REMEDY = "https://txslrempwp.vzwcorp.com/arsys/forms/rem1.vh.eng.vzwcorp.com/VZ+-+East+-+NRB/NRB/?cacheid=186602a3"
+
